11228c0491be5a81e3316918ef7c510cf3d29f0e
Renata Sousa <rods@energy.aau.dk>
2022-04-19 17:41:33 +0200

    Create gpiodemo.py

---
+++ gpiodemo.py
@@ -0,0 +1,21 @@
+from periphery import GPIO
+import time
+
+
+led_pin = 6
+gpio = GPIO(led_pin, "out")
+
+try:
+    while 1:
+        print("turn on led")
+        gpio.write(True)
+        time.sleep(2)
+        print("turn off led")
+        gpio.write(False)
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    gpio.write(False)
+    gpio.close()
+
+print("done")
